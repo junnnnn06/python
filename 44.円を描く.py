da8bb111b811fc3b1f48ce07a5bb0229adbe3de0
junnnnn06 <junnnnn06@icloud.com>
2017-09-22 15:34:51 +0900

    円を描く

---
+++ 44.円を描く.py
@@ -0,0 +1,18 @@
+'''
+matplotlib 円パッチの使用
+'''
+import matplotlib.pyplot as plt
+
+def create_circle():
+    circle = plt.Circle((0,0), radius = 0.5)
+    return circle
+
+def show_shape(patch):
+    ax = plt.gca()
+    ax.add_patch(patch)
+    plt.axis('scaled')
+    plt.show()
+
+if __name__ == '__main__':
+    c = create_circle()
+    show_shape(c)
