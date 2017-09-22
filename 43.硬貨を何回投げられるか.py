2e736f8e57402ae29a2d462a8cd52b3887453b91
junnnnn06 <junnnnn06@icloud.com>
2017-09-22 13:35:21 +0900

    硬貨を何回投げられるか

---
+++ 43.硬貨を何回投げられるか.py
@@ -0,0 +1,26 @@
+'''
+お金がなくなるまで、何回硬貨を投げられるか
+'''
+import random
+
+def play_game(start_amount):
+    win_amount = 1
+    loss_amount = 2
+
+    cur_amount = start_amount
+    toss = 0
+
+    while cur_amount > 0:
+        toss += 1
+        tosses = random.randint(0,1)
+        if tosses == 0:
+            cur_amount += win_amount
+            print('+1点! 現在の所持金は{0}'.format(cur_amount))
+        else:
+            cur_amount -= loss_amount
+            print('-1.5点! 現在の所持金は{0}'.format(cur_amount))
+    print('終了! :(現在の所持金は{0}. 試行回数は{1})'.format(cur_amount, toss))
+
+if __name__ == '__main__':
+    start_amount = float(input('開始時の金額を入力してください: '))
+    play_game(start_amount)
